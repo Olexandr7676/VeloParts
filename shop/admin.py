f0003af1b8501a872e6ceef5e755b8cae7113eec
Ilia Romaniuk <1708illa@gmail.com>
2026-05-26 20:02:59 +0300

    lab 4

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Brand, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # Виводимо колонки в адмінці
+    list_display = ('name', 'created_at', 'updated_at')
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # Тут виводимо навіть більше: до якого бренду і категорії належить товар
+    list_display = ('name', 'category', 'brand', 'price', 'created_at', 'updated_at')
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва категорії")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категорія"
+        verbose_name_plural = "Категорії"
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва бренду")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Бренд"
+        verbose_name_plural = "Бренди"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Назва товару")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Ціна")
+
+    # Ось тут ми ОБ'ЄДНУЄМО таблиці, як просив викладач
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категорія")
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, verbose_name="Бренд")
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товари"
